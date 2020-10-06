7a1a14758434f90901c23e964bc9aff9a99e5df5
Huy Hoang <huyhoangnd237@gmail.com>
2020-10-06 09:43:08 +0700

    Updates

---
+++ precache-manifest.f0fddaf36fccf6f0d877a4c721e9f4d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158e9213efd77ee0d55910131ff3e5e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b463f525957fb5474069",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "40860404e6f6202d7d25",
+    "url": "/static/css/main.6c90e5b2.chunk.css"
+  },
+  {
+    "revision": "b463f525957fb5474069",
+    "url": "/static/js/2.14e34461.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.14e34461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40860404e6f6202d7d25",
+    "url": "/static/js/main.36f72eb8.chunk.js"
+  },
+  {
+    "revision": "8dee9af6c6996aa5f73b",
+    "url": "/static/js/runtime-main.5fa040d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
